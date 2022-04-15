a216b1ed14918d4aa0f8fe94375c974ddf1d42ee
{Serhio} <{sergejklimenkostud@gmail.com}>
2022-04-15 14:35:10 +0300

    Added Empty Backend rep

---
+++ bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/BugdetPlannerApplication.java
@@ -0,0 +1,13 @@
+package lt.vtmc.komanda.bugdetPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugdetPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugdetPlannerApplication.class, args);
+	}
+
+}
---
+++ bugdetPlanner-backend/src/test/java/lt/vtmc/komanda/bugdetPlanner/BugdetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vtmc.komanda.bugdetPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugdetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
