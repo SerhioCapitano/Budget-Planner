06052d82208d5149b948a343c2b407ca49dec558
GITautas <gintautasbutkus@gmail.com>
2022-04-20 11:25:16 +0300

    Initial Frontend Template Commit

---
+++ budgetPlanner-frontend/src/App.js
@@ -0,0 +1,35 @@
+
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import Billing from "./pages/Billing";
+import Rtl from "./pages/Rtl";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+// import "antd/dist/antd.css";
+import 'antd/dist/antd.min.css';
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/billing" component={Billing} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
