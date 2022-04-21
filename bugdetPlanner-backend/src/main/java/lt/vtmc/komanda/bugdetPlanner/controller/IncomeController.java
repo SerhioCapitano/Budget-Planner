e4bb0bf2873d312cfe7d69fcb0153b6cec64405b
{Serhio} <{sergejklimenkostud@gmail.com}>
2022-04-21 11:13:03 +0300

    update Controller to Income

--- bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/controller/IncomeController.java
+++ bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/controller/IncomeController.java
@@ -3,13 +3,19 @@ package lt.vtmc.komanda.bugdetPlanner.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import lt.vtmc.komanda.bugdetPlanner.model.Incomes;
+import lt.vtmc.komanda.bugdetPlanner.exception.ResourceNotFoundException;
+import lt.vtmc.komanda.bugdetPlanner.model.Income;
 import lt.vtmc.komanda.bugdetPlanner.repository.IncomeRepository;
 
 @RestController
@@ -23,14 +29,57 @@ public class IncomeController {
 		this.incomeRepository = incomeRepository;
 	}
 
+	// GET ALL INCOMES IN LIST
+
 	@GetMapping
-	public List<Incomes> getAllIncomes() {
+	public List<Income> getAllIncomes() {
 		return incomeRepository.findAll();
 	}
 
+	// CREATE INCOME
+
 	@PostMapping
-	public Incomes createIncome(@RequestBody Incomes income) {
+	public Income createIncome(@RequestBody Income income) {
 		return incomeRepository.save(income);
+
+	}
+
+	// GET INCOME BY ID
+
+	@GetMapping("{id}")
+	public ResponseEntity<Income> getIncomeById(@PathVariable long id) {
+		Income income = incomeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Income not found with id: " + id));
+		return ResponseEntity.ok(income);
+	}
+
+	// UPDATE INCOME BY ID
+
+	@PutMapping("{id}")
+	public ResponseEntity<Income> updateEmployee(@PathVariable long id, @RequestBody Income incomeDetails) {
+		Income updateIncome = incomeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+		updateIncome.setAmount(incomeDetails.getAmount());
+		updateIncome.setDescription(incomeDetails.getDescription());
+		updateIncome.setTimeStamp(incomeDetails.getTimeStamp());
+
+		incomeRepository.save(updateIncome);
+
+		return ResponseEntity.ok(updateIncome);
+
+	}
+
+	// DELETE INCOME BY ID
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
+		Income employee = incomeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Income not found with id: " + id));
+
+		incomeRepository.delete(employee);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
 	}
 
--- bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/repository/IncomeRepository.java
+++ bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/repository/IncomeRepository.java
@@ -2,8 +2,8 @@ package lt.vtmc.komanda.bugdetPlanner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import lt.vtmc.komanda.bugdetPlanner.model.Incomes;
+import lt.vtmc.komanda.bugdetPlanner.model.Income;
 
-public interface IncomeRepository extends JpaRepository<Incomes, Long> {
+public interface IncomeRepository extends JpaRepository<Income, Long> {
 
 }
