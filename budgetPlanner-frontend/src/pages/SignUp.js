8e61633575e68bca70ea1004f10906f3203d9ade
Vaida <linkuviene.vaida@gmail.com>
2022-05-10 16:23:00 +0300

    Security without navigation

--- budgetPlanner-frontend/src/pages/SignUp.js
+++ budgetPlanner-frontend/src/pages/SignUp.js
@@ -182,9 +182,9 @@ export default class SignUp extends Component {
             <div className="header-col header-nav">
               <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
                 <Menu.Item key="1">
-                  <Link to="/suvestinė">
+                  <Link to="/santrauka">
                     {template}
-                    <span> Suvestinė</span>
+                    <span> Santrauka</span>
                   </Link>
                 </Menu.Item>
                 {/* <Menu.Item key="2">
--- budgetPlanner-frontend/src/services/IncomesService.js
+++ budgetPlanner-frontend/src/services/IncomesService.js
@@ -1,35 +1,32 @@
 import axios from 'axios';
 import authHeader from "./auth-header";
 
+
 const INCOMES_BASE_REST_API_URL = 'http://localhost:8080/api/v1/incomes';
 
 
 class IncomesService {
     
     getAllIncomes() {
-        return axios.get(INCOMES_BASE_REST_API_URL, {headers: authHeader()})
+        return axios.get(INCOMES_BASE_REST_API_URL, { headers: authHeader() })
     }
 
     createIncome(income) {
-        return axios.post(INCOMES_BASE_REST_API_URL, income, {headers: authHeader()})
+        return axios.post(INCOMES_BASE_REST_API_URL, income, { headers: authHeader() })
     }
 
     deleteIncome(incomeId) {
-        return axios.delete(INCOMES_BASE_REST_API_URL + '/' + incomeId, {headers: authHeader()});
+        return axios.delete(INCOMES_BASE_REST_API_URL + '/' + incomeId, { headers: authHeader() });
     }
 
 
     updateIncome(incomeId,income) {
-        return axios.put(INCOMES_BASE_REST_API_URL + '/' + incomeId,income, {headers: authHeader()});
+        return axios.put(INCOMES_BASE_REST_API_URL + '/' + incomeId,income), { headers: authHeader() };
     }
 
     getIncomeById(incomeId) {
-        return axios.get(INCOMES_BASE_REST_API_URL + '/' + incomeId, {headers: authHeader()});
+        return axios.get(INCOMES_BASE_REST_API_URL + '/' + incomeId, { headers: authHeader() });
     }
-
-
-
-
 
 }
 
