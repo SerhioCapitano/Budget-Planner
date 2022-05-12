4d62a5835d2a128100419e7718fe7237048c2e1b
Vaida <linkuviene.vaida@gmail.com>
2022-05-12 16:12:01 +0300

    Users table

--- budgetPlanner-frontend/src/services/UserService.js
+++ budgetPlanner-frontend/src/services/UserService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import authHeader from "./auth-header";
 
 const USERS_BASE_REST_API_URL = 'http://localhost:8080/api/v1/users';
 
@@ -6,23 +7,23 @@ const USERS_BASE_REST_API_URL = 'http://localhost:8080/api/v1/users';
 class UserService {
     
     getAllUsers() {
-        return axios.get(USERS_BASE_REST_API_URL)
+        return axios.get(USERS_BASE_REST_API_URL,  { headers: authHeader() })
     }
 
     createUsers(user) {
-        return axios.post(USERS_BASE_REST_API_URL, user)
+        return axios.post(USERS_BASE_REST_API_URL, user,  { headers: authHeader() })
     }
 
     deleteUsers(userId) {
-        return axios.delete(USERS_BASE_REST_API_URL + '/' + userId);
+        return axios.delete(USERS_BASE_REST_API_URL + '/' + userId,  { headers: authHeader() });
     }
 
     updateUsers(userId, user) {
-        return axios.put(USERS_BASE_REST_API_URL + '/' + userId, user);
+        return axios.put(USERS_BASE_REST_API_URL + '/' + userId, user,  { headers: authHeader() });
     }
 
     getUsersById(userId) {
-        return axios.get(USERS_BASE_REST_API_URL + '/' + userId);
+        return axios.get(USERS_BASE_REST_API_URL + '/' + userId,  { headers: authHeader() });
     }
 
 
