385eb486259ddedb5b69a28b34aa4f2b2a867807
{Serhio} <95015159+SerhioCapitano@users.noreply.github.com>
2022-05-12 19:34:35 +0300

    Users Table front updated

--- bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/repository/UserRepository.java
+++ bugdetPlanner-backend/src/main/java/lt/vtmc/komanda/bugdetPlanner/repository/UserRepository.java
@@ -17,6 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	User findByUsername(String username);
 	Boolean existsByUsername(String username);
 	Boolean existsByEmail(String email);
-	User deleteByUsername(String username);
+	void deleteByUsername(String username);
 	
 }
